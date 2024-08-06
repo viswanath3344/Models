b51b8d80c158c9ff7fbca2095368d6425b8172a1
Viswanath Reddy Ponthota (Contractor) <viswanathreddy.ponthota@cognizant.com>
2024-08-06 21:04:10 +0530

    Dummy Model for Package.

--- Sources/Models/Models.swift
+++ Sources/Models/Models.swift
@@ -1,2 +1,17 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import Foundation
+
+public struct User: Identifiable {
+    public var id = UUID()
+    var name: String
+    var age: Int
+    
+   public init(id: UUID = UUID(), name: String, age: Int) {
+        self.id = id
+        self.name = name
+        self.age = age
+    }
+    
+}
